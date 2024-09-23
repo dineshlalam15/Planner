5a5686229952df6feed6c2c1b814ee5b7b18e876
dinesh <dineshlalam15@gmail.com>
2024-09-23 08:12:13 +0530

    Google Authentication added

--- src/utils/app.js
+++ src/utils/app.js
@@ -2,14 +2,52 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
 import userRouter from '../routers/user.router.js';
 import taskRouter from '../routers/task.router.js';
+import './passport.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+    return res.send("<button><a href='/auth'>Login With Google</a></button>")
+});
+
+app.get('/auth' , passport.authenticate('google', { 
+        scope: [ 'email', 'profile' ]
+    })
+);
+
+app.get("/login",
+    passport.authenticate( 'google', {
+        successRedirect: '/login/success',
+        failureRedirect: '/login/failure'
+    }),
+);
+
+app.get('/login/success' , (req , res) => {
+    if(!req.user){
+        return res.redirect('/auth/callback/failure');
+    }
+    return res.send("Welcome " + req.user.email);
+});
+
+app.get('/login/failure' , (req , res) => {
+    return res.send("Error");
+})
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tasks', taskRouter);
--- src/utils/passport.js
+++ src/utils/passport.js
@@ -1,53 +1,40 @@
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
-import User from '../models/user.model.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import passport from "passport";
+import { Strategy as GoogleStartegy } from "passport-google-oauth2";
+import User from "../models/user.model.js";
 
 passport.use(
-    new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:4000/auth/callback",
-        passReqToCallback: true
+  new GoogleStartegy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      passReqToCallback: true,
     },
-    async function(request, accessToken, refreshToken, profile, done) {
-        try {
-            let findUser = await User.findOne({ email: profile.emails[0].value });    
-            if (!findUser) {
-                findUser = await User.create({
-                    name: {
-                        firstName: profile.name.givenName || "",
-                        lastName: profile.name.familyName || "",
-                    },
-                    email: profile.emails[0].value,
-                    displayPicture: profile.photos[0].value,
-                    refreshToken: refreshToken,
-                    accessToken: accessToken, 
-                    isOAuthUser: true,
-                });
-            } else {
-                findUser.refreshToken = refreshToken;
-                findUser.accessToken = accessToken;
-                await findUser.save({ validateBeforeSave: false });              
-            }
-            return done(null, findUser);
-        } catch (error) {
-            console.error('Error in GoogleStrategy:', error);
-            return done(error, null);
-        }
-    })
+    async function (request, accessToken, refreshToken, profile, done) {
+      const exist = await User.findOne({ email: profile["emails"][0].value });
+      if (!exist) {
+        await User.create({
+          name: {
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+          },
+          email: profile.emails[0].value,
+          displayPicture: profile.photos[0].value,
+          refreshToken: refreshToken,
+          accessToken: accessToken,
+          isOAuthUser: true,
+        });
+      }
+      const user = await User.findOne({ email: profile.emails[0].value });
+      return done(null, user);
+    }
+  )
 );
 
-passport.serializeUser((user, done) => {
-    done(null, user._id);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error, null);
-    }
+  
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
